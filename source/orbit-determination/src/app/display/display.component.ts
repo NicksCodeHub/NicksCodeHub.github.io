e1b187f752da57af4b6477b45134e164d5406d03
Nicholas Myers <nickmyers0@hotmail.com>
2020-03-21 14:11:42 -0400

    Added mouse events an an orbit tracker taken from the examples.

--- source/orbit-determination/src/app/display/display.component.ts
+++ source/orbit-determination/src/app/display/display.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import * as THREE from 'three';
-import { Vector2 } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-display',
@@ -13,8 +13,12 @@ export class DisplayComponent {
   renderer = new THREE.WebGLRenderer();
   scene = null;
   camera = null;
+  controls = null;
+
   mesh1 = null;
   mesh2 = null;
+
+  mouseDown = false;
 
   constructor() { 
     this.scene = new THREE.Scene();
@@ -24,11 +28,21 @@ export class DisplayComponent {
     this.camera.position.y = 30;
     this.camera.lookAt(new THREE.Vector3(0,0,0));
 
-    let geometry = new THREE.SphereGeometry(10,25,25);
+    this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+    this.controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    this.controls.dampingFactor = 0.05;
+
+    this.controls.enablePan = false;
+    this.controls.screenSpacePanning = false;
+    this.controls.minDistance = 15;
+    this.controls.maxDistance = 500;
+    
+
+    let geometry = new THREE.SphereGeometry(1,15,15);
     const material1 = new THREE.MeshBasicMaterial({color: 0x2099aa, wireframe: false});
     this.mesh1 = new THREE.Mesh(geometry, material1);
 
-    geometry = new THREE.SphereGeometry(10.1,25,25);
+    geometry = new THREE.SphereGeometry(1,15,15);
     const material2 = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
     this.mesh2 = new THREE.Mesh(geometry, material2);
 
@@ -46,6 +60,8 @@ export class DisplayComponent {
   animate() {
       window.requestAnimationFrame(() => this.animate());
 
+      this.controls.update();
+
       this.mesh1.rotation.y += 0.01;
       this.mesh2.rotation.y += 0.01;
       this.renderer.render(this.scene, this.camera);
@@ -55,5 +71,19 @@ export class DisplayComponent {
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  onMouseWheel(e) {
+  }
+
+  onMouseUp(e) {
+    this.mouseDown = false;
+  }
+
+  onMouseDown(e) {
+    this.mouseDown = true;
+  }
+  
+  onMouseMove(e) {
   }
 }
